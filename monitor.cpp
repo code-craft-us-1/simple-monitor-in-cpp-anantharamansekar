5f0a159746252aaad06f6d18517f3d137c174c7c
Sekar, Anantharaman <Anantharaman.Sekar@philips.com>
2024-09-30 10:26:33 +0530

    Fix lint errors

--- monitor.cpp
+++ monitor.cpp
@@ -8,8 +8,7 @@ using std::flush;
 using std::this_thread::sleep_for;
 using std::chrono::seconds;
 
-void PrintError(std::string errString)
-{
+void PrintError(std::string errString) {
     cout << errString << std::endl;
     for (int i = 0; i < 6; i++) {
         cout << "\r* " << flush;
@@ -19,56 +18,53 @@ void PrintError(std::string errString)
     }
 }
 
-int CheckTemperature(float temperature)
-{
-	int result = 1;
+int CheckTemperature(float temperature) {
+    int result = 1;
 
-	if (temperature > 102 || temperature < 95) {
-		PrintError("Temperature is critical!");
-		result = 0;
-	}
+    if (temperature > 102 || temperature < 95) {
+        PrintError("Temperature is critical!");
+        result = 0;
+    }
 
-	return result;
+    return result;
 }
 
-int CheckPulse(float pulseRate)
-{
-	int result = 1;
+int CheckPulse(float pulseRate) {
+    int result = 1;
 
-	if (pulseRate < 60 || pulseRate > 100) {
-		PrintError("Pulse Rate is out of range!");
-		result = 0;
-	}
+    if (pulseRate < 60 || pulseRate > 100) {
+        PrintError("Pulse Rate is out of range!");
+        result = 0;
+    }
 
-	return result;
+    return result;
 }
 
-int CheckSPO2(float spo2)
-{
-	int result = 1;
+int CheckSPO2(float spo2) {
+    int result = 1;
 
-	if (spo2 < 90) {
-		PrintError("Oxygen Saturation out of range!");
-		result = 0;
-	}
+    if (spo2 < 90) {
+        PrintError("Oxygen Saturation out of range!");
+        result = 0;
+    }
 
-	return result;
+    return result;
 }
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-	int result = 1;
+    int result = 1;
 
-	result = CheckTemperature(temperature);
+    result = CheckTemperature(temperature);
 
-	if (result)
-	{
-		result = CheckPulse(pulseRate);
-	}
+    if (result)
+    {
+        result = CheckPulse(pulseRate);
+    }
 
-	if (result)
-	{
-		result = CheckSPO2(spo2);
-	}
-	
-  return result;
+    if (result)
+    {
+        result = CheckSPO2(spo2);
+    }
+
+    return result;
 }
